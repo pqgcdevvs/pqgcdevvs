85899b81443b563c626974b48b6cddf83dd4e1eb
pqgcdevvs <168045437+pqgcdevvs@users.noreply.github.com>
2024-04-24 21:07:21 -0400

    Create README.lua

---
+++ README.lua
@@ -0,0 +1,7 @@
+local profile = {
+    name = "pqgc/page",
+    role = "Roblox Developer",
+    robloxUserID = 5126960265,
+    website = "https://pqgc.carrd.co/",
+    languagesLearning = {"JavaScript", "CSS", "HTML", "JSON", "Luau"},
+        }
