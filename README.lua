87ffe9af8a2830e48defca025d0501f50fb18a12
pqgc <168045437+pqgcdevvs@users.noreply.github.com>
2024-10-22 15:15:36 -0400

    when the

--- README.lua
+++ README.lua
@@ -1,7 +1,1 @@
-local profile = {
-    name = "pqgc/page",
-    role = "Roblox Developer",
-    robloxUserID = 5126960265,
-    website = "https://pqgc.carrd.co/",
-    languagesLearning = {"JavaScript", "CSS", "HTML", "JSON", "Luau"},
-        }
+when the
